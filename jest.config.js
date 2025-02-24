f229d20fd186cac0baf56787de9afd15cda7f82f
Florian Rappl <florian.rappl@smapiot.com>
2025-02-24 11:28:20 +0100

    Migrated to vitest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  setupFiles: [],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testPathIgnorePatterns: ['/node_modules/'],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts)$',
-  moduleFileExtensions: ['ts', 'js', 'json', 'node'],
-};
